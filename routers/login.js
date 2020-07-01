cf004d31e2f441ab75e2a050dc96a47ec7330a4e
yang <2967157611@qq.com>
2020-07-01 10:18:40 +0800

    完成注册接口用户

--- routers/login.js
+++ routers/login.js
@@ -3,7 +3,8 @@
 */
 const express = require('express')
 const path = require('path')
-const db = require(path.join(__dirname,'../common/common.js'))
+const utility = require('utility')
+const db = require(path.join(__dirname, '../common/common.js'))
 
 //产生一个router对象
 const router = express.Router()
@@ -16,27 +17,35 @@ router.post('/login', (req, res) => {
 //注册用户接口
 router.post('/reguser', async (req, res) => {
     //获取前端换地过来的参数
-    let param = req.body
+    let param = req.body//{ username: 'tom', password: 'Jerry' }
+    //对客户端传递过来的数据进行加密后再进行数据库的操作
+    param.password = utility.md5(req.body.password)
     //调用数据库的相关方法进行数据的添加操作
     let sql = 'insert into user set ?'
     let ret = await db.operateDate(sql, param)
-    //非空判断和
-    if(ret && ret.affectedRows > 0){
-
+    //非空判断和操作影响了几行数据判断
+    if (ret && ret.affectedRows > 0) {
+        res.json({
+            status: 0,
+            message: "注册成功"
+        })
+    } else {
+        res.json({
+            status: 1,
+            message: "注册失败"
+        })
     }
-    console.log(ret)
-    res.send('reguser')
 })
 
 router.get('/test', async (req, res) => {
+    console.log('hello')
     let sql = 'select * from user'
     let ret = await db.operateDate(sql, null)
     res.json({
-      status: 0,
-      data: ret
+        status: 0,
+        data: ret
     })
-    res.send('abcd')
-  })
+})
 
 //导出router对象 (router本来就是对象)
 module.exports = router
