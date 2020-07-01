5bd93e7b20878208b3f4c47155b0b82153fc47c9
yang <2967157611@qq.com>
2020-07-01 22:33:36 +0800

    登录注册模块完成

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const cors = require('cors')
 const path = require('path')
-const loginUrl = require(path.join(__dirname,"./routers/login.js"))
+const jwt = require('express-jwt')
+const loginUrl = require(path.join(__dirname, "./routers/login.js"))
 
 //2.创建服务器
 let app = express();
@@ -16,10 +17,20 @@ app.use(express.json())
 //设置跨域
 app.use(cors())
 
+//设置token
+app.use(jwt({
+    //加密的唯一标识(加密的干扰字符串)
+    secret: 'bigevent'
+}).unless({
+    //unless的作用 : 排除一些路径不需要进行token解析
+    path: ['/api/login', '/api/reguser','/api/test']
+}));
+
 //设置路径
-app.use('/api',loginUrl)
+app.use('/api', loginUrl)
 
 //3.开启服务器
 app.listen(9999, () => {
     console.log('running...');
 })
+
--- routers/login.js
+++ routers/login.js
@@ -3,15 +3,50 @@
 */
 const express = require('express')
 const path = require('path')
-const utility = require('utility')
+const utils = require('utility')
+const jwt = require('jsonwebtoken')
 const db = require(path.join(__dirname, '../common/common.js'))
 
 //产生一个router对象
 const router = express.Router()
 
 //把路由挂载到router对象上
-router.post('/login', (req, res) => {
-    res.send('login')
+//用户登录接口
+router.post('/login', async (req, res) => {
+    //获取前端换地过来的参数
+    let param = req.body
+    //对客户端传递过来的数据进行加密后再进行数据库的操作
+    param.password = utils.md5(req.body.password)
+    //根据用户密码查询数据库
+    let sql = 'select id from user where username = ? and password = ?'
+    //operateDate的第二个参数是'?'占位符 如果是查询,那么这里是数组,如果是增删改查,那么这里是对象
+    let ret = await db.operateDate(sql, [param.username, param.password])
+    //非空判断和是否传参判断
+    if (ret && ret.length > 0) {
+        //如果登录验证通过,就生成token信息
+        //1. 参数1表示添加到token中的信息
+        //2. 参数2表示加密的唯一标识(加密的干扰字符串)
+        //3. 加密配置选项(可以设置token的有效期)
+        //jwt规定在token字符串之前添加一个Bearer 特殊标识
+        //"BearereyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IjEyMzQiLCJpZCI6MTUsImlhdCI6MTU5MzYxMDA5NywiZXhwIjoxNTkzNjEzNjk3fQ.jgzFGrzli5e2GtFGGCVKVTahDgHtssYs4zCvr1T4_FM"
+        let token = jwt.sign({
+            username: param.username,
+            id: ret[0].id
+        }, 'bigevent', {
+            //token有效期
+            expiresIn: '1h'
+        })
+        res.json({
+            status: 0,
+            message: '登录成功',
+            token: 'Bearer ' + token
+        })
+    } else {
+        res.json({
+            status: 1,
+            message: '登录失败'
+        })
+    }
 })
 
 //注册用户接口
@@ -19,10 +54,11 @@ router.post('/reguser', async (req, res) => {
     //获取前端换地过来的参数
     let param = req.body//{ username: 'tom', password: 'Jerry' }
     //对客户端传递过来的数据进行加密后再进行数据库的操作
-    param.password = utility.md5(req.body.password)
+    param.password = utils.md5(req.body.password)
     //调用数据库的相关方法进行数据的添加操作
     let sql = 'insert into user set ?'
     let ret = await db.operateDate(sql, param)
+    console.log(ret);
     //非空判断和操作影响了几行数据判断
     if (ret && ret.affectedRows > 0) {
         res.json({
